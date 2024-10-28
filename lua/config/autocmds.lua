da589b8f403d3a5dd06f9733aedc1b3b30e1e36c
Baptiste GAZE <bpt.gaze@gmail.com>
2024-10-28 19:38:28 +0100

    plug ruby-lsp

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,8 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+
+-- Rubocop
 vim.opt.signcolumn = "yes"
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "ruby",
@@ -12,3 +14,45 @@ vim.api.nvim_create_autocmd("FileType", {
     }
   end,
 })
+
+-- RubyLSP
+local lspconfig = require("lspconfig")
+local configs = require("lspconfig.configs")
+local util = require("lspconfig.util")
+
+if not configs.ruby_lsp then
+	local enabled_features = {
+		"documentHighlights",
+		"documentSymbols",
+		"foldingRanges",
+		"selectionRanges",
+		-- "semanticHighlighting",
+		"formatting",
+		"codeActions",
+	}
+
+	configs.ruby_lsp = {
+		default_config = {
+			cmd = { "bundle", "exec", "ruby-lsp" },
+			filetypes = { "ruby" },
+			root_dir = util.root_pattern("Gemfile", ".git"),
+			init_options = {
+				enabledFeatures = enabled_features,
+			},
+			settings = {},
+		},
+		commands = {
+			FormatRuby = {
+				function()
+					vim.lsp.buf.format({
+						name = "ruby_lsp",
+						async = true,
+					})
+				end,
+				description = "Format using ruby-lsp",
+			},
+		},
+	}
+end
+
+lspconfig.ruby_lsp.setup({ on_attach = on_attach, capabilities = capabilities })
