ae01b07f9c0bea6a101bfd050f2c45027def7a75
Baptiste GAZE <bpt.gaze@gmail.com>
2024-10-30 10:18:35 +0100

    switch back to ruby-lsp with mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,20 +11,33 @@ return {
         bashls = {
           filetypes = { "sh", "zsh" },
         },
-        -- ruby_lsp = {
-        --   mason = false,
-        --   cmd = { "bundle", "exec", "ruby-lsp" },
-        --   init_options = {
-        --     formatter = "auto",
-        --   },
-        -- },
+        ruby_lsp = {
+          -- mason = false,
+          -- cmd = { "ruby-lsp" },
+          init_options = {
+            formatter = "auto",
+          },
+        },
         -- See: https://docs.rubocop.org/rubocop/usage/lsp.html
         -- rubocop = {
         --   mason = false,
         --   cmd = { "bundle", "exec", "rubocop", "--lsp" },
         --   root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
         -- },
-        solargraph = {},
+        -- solargraph = {
+        --   mason = false,
+        --   -- cmd = { "bundle", "exec", "solargraph", "stdio" },
+        --   settings = {
+        --     autoformat = false,
+        --     formatting = false,
+        --     completion = true,
+        --     diagnostic = true,
+        --     folding = true,
+        --     references = true,
+        --     rename = true,
+        --     symbols = true,
+        --   },
+        -- },
         -- sqlls = {},
         -- terraformls = {},
         -- tsserver = {},
