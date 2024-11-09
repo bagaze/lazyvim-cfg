de2f49a23c61810a551c5e01659ea1b5c23173c5
Baptiste GAZE <bpt.gaze@gmail.com>
2024-11-09 14:28:59 +0100

    add cfg file for snacks - terminal

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+      terminal = {
+        win = {
+          -- can be set to "bottom", "top", "float"
+          -- position = "bottom",
+        },
+      },
+  },
+}
