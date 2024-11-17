76f192618d865df40f9916d22f8678ce48d0c600
Baptiste GAZE <bpt.gaze@gmail.com>
2024-11-17 10:29:17 +0100

    update cfg for wezterm

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,11 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set({ 'n', 'v' }, '<D-c>', '"+y', { desc = "Copy to clipboard" }) -- cmd+c to copy to system clipboard
-vim.keymap.set({ 'n', 'v' }, '<D-v>', '"+p', { desc = "Paste from clipboard" }) -- cmd+v to paste from system clipboard
-vim.keymap.set({ 'n', 'v' }, '<D-v>', '"+p', { desc = "Paste from clipboard" }) -- cmd+v to paste from system clipboard
+-- For wezterm
+vim.keymap.set({ "n", "v" }, "<Char-0xAA>", '"+y', { desc = "Copy to clipboard" }) -- cmd+c to copy to system clipboard
+vim.keymap.set({ "i", "x", "n", "s" }, "<Char-0xAB>", function()
+  Snacks.bufdelete()
+end, { desc = "Close Buffer" })
+-- For Kitty
+vim.keymap.set({ "n", "v" }, "<D-c>", '"+y', { desc = "Copy to clipboard" }) -- cmd+c to copy to system clipboard
+vim.keymap.set({ "n", "v" }, "<D-v>", '"+p', { desc = "Paste from clipboard" }) -- cmd+v to paste from system clipboard
 vim.keymap.set({ "i", "x", "n", "s" }, "<D-p>", ":Telescope find_files<ESC>", { desc = "Find files" })
 vim.keymap.set({ "i", "x", "n", "s" }, "<D-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 -- From: https://stackoverflow.com/a/59029500
-vim.keymap.set('n', '#', '#N')
-vim.keymap.set('n', '*', '*N')
+vim.keymap.set("n", "#", "#N")
+vim.keymap.set("n", "*", "*N")
