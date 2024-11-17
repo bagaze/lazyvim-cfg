5928d7fec8be5e7ea9d529116dc852450b68c487
Baptiste GAZE <bpt.gaze@gmail.com>
2024-11-17 21:14:04 +0100

    add lazydocker

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,6 +10,8 @@ end, { desc = "Close Buffer" })
 -- For Kitty
 vim.keymap.set({ "n", "v" }, "<D-c>", '"+y', { desc = "Copy to clipboard" }) -- cmd+c to copy to system clipboard
 vim.keymap.set({ "n", "v" }, "<D-v>", '"+p', { desc = "Paste from clipboard" }) -- cmd+v to paste from system clipboard
+
+vim.keymap.set("n", "<leader>k", "<cmd>LazyDocker<CR>", { desc = "Toggle LazyDocker", noremap = true, silent = true })
 vim.keymap.set({ "i", "x", "n", "s" }, "<D-p>", ":Telescope find_files<ESC>", { desc = "Find files" })
 vim.keymap.set({ "i", "x", "n", "s" }, "<D-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 -- From: https://stackoverflow.com/a/59029500
