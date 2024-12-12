7660b9105d812efcd7bcb9b8029dbbc8479e1f1d
Baptiste GAZE <bpt.gaze@gmail.com>
2024-12-12 14:48:39 +0100

    clean themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,22 +1,6 @@
 return {
   -- Themes
-  { "maxmx03/solarized.nvim" },
-  { "shaunsingh/moonlight.nvim" },
-  { "ellisonleao/gruvbox.nvim" },
-  { "sainnhe/gruvbox-material" },
-  { "olimorris/onedarkpro.nvim" },
-  {
-    "sainnhe/sonokai",
-    -- lazy = false,
-    -- priority = 1000,
-  },
-  { "tanvirtin/monokai.nvim" },
   { "Mofiqul/dracula.nvim" },
-  { "ofirgall/ofirkai.nvim" },
-  { "sainnhe/everforest" },
-  { "oxfist/night-owl.nvim" },
-  { "Shatur/neovim-ayu" },
-  { "EdenEast/nightfox.nvim" },
   {
     "rose-pine/neovim",
     name = "rose-pine",
