ff3f5a75a6cf00bf6b3a2fb9489ff082c117cb53
Baptiste GAZE <bpt.gaze@gmail.com>
2025-02-03 21:33:58 +0100

    setup smart-splits

---
+++ lua/plugins/kitty.lua
@@ -0,0 +1,65 @@
+return {
+  {
+    "mrjones2014/smart-splits.nvim",
+    lazy = false,
+    -- build = "./kitty/install-kittens.bash",
+    keys = {
+      {
+        "<C-h>",
+        function()
+          require("smart-splits").move_cursor_left()
+        end,
+        desc = "Move to left window",
+      },
+      {
+        "<C-l>",
+        function()
+          require("smart-splits").move_cursor_right()
+        end,
+        desc = "Move to right window",
+      },
+      {
+        "<C-j>",
+        function()
+          require("smart-splits").move_cursor_down()
+        end,
+        desc = "Move to below window",
+      },
+      {
+        "<C-k>",
+        function()
+          require("smart-splits").move_cursor_up()
+        end,
+        desc = "Move to above window",
+      },
+      {
+        "<A-h>",
+        function()
+          require("smart-splits").resize_left()
+        end,
+        desc = "Resize left",
+      },
+      {
+        "<A-l>",
+        function()
+          require("smart-splits").resize_right()
+        end,
+        desc = "Resize right",
+      },
+      {
+        "<A-j>",
+        function()
+          require("smart-splits").resize_down()
+        end,
+        desc = "Resize down",
+      },
+      {
+        "<A-k>",
+        function()
+          require("smart-splits").resize_up()
+        end,
+        desc = "Resize up",
+      },
+    },
+  },
+}
