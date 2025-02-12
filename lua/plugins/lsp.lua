fc44515181f2eca45c6b0036be3ffe9905a7d14f
Baptiste GAZE <bpt.gaze@gmail.com>
2025-02-12 08:34:36 +0100

    update lazy-lock

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -45,12 +45,10 @@ return {
       },
     },
   },
-  {
-    "zeioth/garbage-day.nvim",
-    dependencies = "neovim/nvim-lspconfig",
-    event = "VeryLazy",
-    opts = {
-      -- your options here
-    },
-  },
+  -- {
+  --   "zeioth/garbage-day.nvim",
+  --   dependencies = "neovim/nvim-lspconfig",
+  --   event = "VeryLazy",
+  --   opts = {},
+  -- },
 }
