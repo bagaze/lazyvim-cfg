eac706ad94d3501538a4139df8e6ecef7eb0bd4c
Baptiste GAZE <bpt.gaze@gmail.com>
2025-02-14 18:28:12 +0100

    ai: switch to codecompanion

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,63 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "j-hui/fidget.nvim",
+    },
+    opts = {
+      adapters = {
+        copilot = function()
+          return require("codecompanion.adapters").extend("copilot", {
+            schema = {
+              model = {
+                default = "claude-3.5-sonnet",
+              },
+            },
+          })
+        end,
+      },
+      strategies = {
+        chat = {
+          adapter = "copilot",
+        },
+        inline = {
+          adapter = "copilot",
+        },
+        agent = {
+          adapter = "copilot",
+        },
+      },
+      opts = {
+        -- Set debug logging
+        -- log_level = "DEBUG",
+      },
+    },
+    keys = {
+      -- { "<c-s>", "<CR>", ft = "codecompanion", desc = "Submit Prompt", remap = true },
+      { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+      {
+        "<leader>aa",
+        "<cmd>CodeCompanionChat Toggle<cr>",
+        desc = "Toggle (CodeCompanion)",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>aq",
+        "<cmd>CodeCompanion",
+        desc = "Quick Chat (CodeCompanion)",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>ap",
+        "<cmd>CodeCompanionActions<cr>",
+        desc = "Prompt Actions (CodeCompanion)",
+        mode = { "n", "v" },
+      },
+    },
+    init = function()
+      require("plugins.codecompanion.fidget-spinner"):init()
+    end,
+  },
+}
