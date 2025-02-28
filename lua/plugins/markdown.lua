117f409b2bf325922acf0f2755e043bfd19bdc4f
Baptiste GAZE <bpt.gaze@gmail.com>
2025-02-28 14:28:42 +0100

    add render-markdown

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,27 @@
+return {
+  -- {
+  --   "OXY2DEV/markview.nvim",
+  --   lazy = false,
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "nvim-tree/nvim-web-devicons",
+  --   },
+  --   opts = {
+  --     preview = {
+  --       icon_provider = "mini", -- "devicons" or "mini" or "internal"
+  --     },
+  --   },
+  -- },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "quarto" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+}
