652807a349460e73caca002dbb2a85e2ddb74c57
Baptiste GAZE <bpt.gaze@gmail.com>
2025-03-07 16:28:30 +0100

    add example cfg for statuscolumn customization

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -39,3 +39,31 @@ vim.fn.setenv("MACOSX_DEPLOYMENT_TARGET", "14.7")
 opt.clipboard = ""
 -- Don't make popup menu transparent
 opt.pumblend = 0
+
+-- Customize line number appearance
+-- vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter" }, {
+--   callback = function()
+--     local separator = " ▎ "
+--     vim.opt.statuscolumn = '%s%=%#LineNr4#%{(v:relnum >= 4)?v:relnum."'
+--       .. separator
+--       .. '":""}'
+--       .. '%#LineNr3#%{(v:relnum == 3)?v:relnum."'
+--       .. separator
+--       .. '":""}'
+--       .. '%#LineNr2#%{(v:relnum == 2)?v:relnum."'
+--       .. separator
+--       .. '":""}'
+--       .. '%#LineNr1#%{(v:relnum == 1)?v:relnum."'
+--       .. separator
+--       .. '":""}'
+--       .. '%#LineNr0#%{(v:relnum == 0)?v:lnum." '
+--       .. separator
+--       .. '":""}'
+--
+--     vim.cmd("highlight LineNr0 guifg=#dedede")
+--     vim.cmd("highlight LineNr1 guifg=#bdbdbd")
+--     vim.cmd("highlight LineNr2 guifg=#9c9c9c")
+--     vim.cmd("highlight LineNr3 guifg=#7b7b7b")
+--     vim.cmd("highlight LineNr4 guifg=#5a5a5a")
+--   end,
+-- })
