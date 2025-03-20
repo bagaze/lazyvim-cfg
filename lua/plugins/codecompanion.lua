5d8dec03e0d66666753f153f350e868f88bb2982
Baptiste GAZE <bpt.gaze@gmail.com>
2025-03-20 17:13:57 +0100

    update: codecompanion - use latest release instead of latest commit

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,6 +1,7 @@
 return {
   {
     "olimorris/codecompanion.nvim",
+    version = "*", -- use latest release instead of latest commit
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
