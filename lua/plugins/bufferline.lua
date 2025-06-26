944485ca02d46f0cf22c69b7f60f2ed09eb7dd19
Baptiste GAZE <bpt.gaze@gmail.com>
2025-06-26 14:17:26 +0200

    update: bufferline insert_at_end

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        -- See: https://github.com/akinsho/bufferline.nvim/blob/655133c3b4c3e5e05ec549b9f8cc2894ac6f51b3/doc/bufferline.txt#L168
+        sort_by = "insert_at_end",
+      },
+    },
+  },
+}
