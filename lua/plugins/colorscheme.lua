ecff2e0e5560613ebe128e4fac3a70b39cfe771d
Baptiste GAZE <bpt.gaze@gmail.com>
2025-07-22 22:59:54 +0200

    add: kanagawa colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -83,6 +83,55 @@ return {
     },
   },
   {
+    "rebelot/kanagawa.nvim",
+    enabled = false,
+    -- lazy = true,
+    -- priority = 1000,
+    opts = {
+      transparent = true,
+      dimInactive = false,
+      -- From: https://github.com/rebelot/kanagawa.nvim?tab=readme-ov-file#transparent-floating-windows
+      overrides = function(colors)
+        local theme = colors.theme
+        local makeDiagnosticColor = function(color)
+          local c = require("kanagawa.lib.color")
+          return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
+        end
+        return {
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
+
+          -- Save an hlgroup with dark background and dimmed foreground
+          -- so that you can use it where your still want darker windows.
+          -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+          -- Popular plugins that open floats will link to NormalFloat by default;
+          -- set their background accordingly if you wish to keep them dark and borderless
+          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+          -- Dark completion (popup) menu
+          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency,,
+          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+          PmenuSbar = { bg = theme.ui.bg_m1 },
+          PmenuThumb = { bg = colors.palette.boatYellow2 },
+          BlinkCmpMenuBorder = { fg = "", bg = "" },
+
+          -- Tint background of diagnostic messages with their foreground color
+          DiagnosticVirtualTextHint = makeDiagnosticColor(theme.diag.hint),
+          DiagnosticVirtualTextInfo = makeDiagnosticColor(theme.diag.info),
+          DiagnosticVirtualTextWarn = makeDiagnosticColor(theme.diag.warning),
+          DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
+
+          -- Visual
+          Visual = { fg = colors.palette.waveRed, bg = theme.ui.bg_p3 },
+        }
+      end,
+    },
+  },
+  {
     "rose-pine/neovim",
     name = "rose-pine",
     lazy = true,
