bf0075060595e269c67de1f0030c9959bff764ce
Baptiste GAZE <bpt.gaze@gmail.com>
2025-08-08 10:50:24 +0200

    disable: avante

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -193,6 +193,7 @@ return {
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
+    enabled = false,
     -- cmd = { "AvanteToggle" },
     -- lazy = true,
     version = false, -- set this if you want to always pull the latest change
