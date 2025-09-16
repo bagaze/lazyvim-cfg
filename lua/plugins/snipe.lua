c75637504932cc47c8a10b3f6d89b4c28b0c3bc6
Baptiste GAZE <bpt.gaze@gmail.com>
2025-09-16 10:39:29 +0200

    update: disable bufferline (barbar) and leverage snipe instead

--- lua/plugins/snipe.lua
+++ lua/plugins/snipe.lua
@@ -19,8 +19,22 @@ return {
           ---@type "single"|"rounded"
           border = "rounded", -- use "single" for single border
         },
+        -- Preselect the currently open buffer
+        ---@type boolean
+        preselect_current = true,
+        -- Changes how the items are aligned: e.g. "<tag> foo    " vs "<tag>    foo"
+        -- Can be "left", "right" or "file-first"
+        -- NOTE: "file-first" puts the file name first and then the directory name
+        ---@type "left"|"right"|"file-first"
+        text_align = "file-first",
       },
       navigate = {
+        -- You can also just use normal navigation to go to the item you want
+        -- this option just sets the keybind for selecting the item under the
+        -- cursor
+        under_cursor = "L",
+        -- In case you changed your mind, provide a keybind that lets you
+        -- cancel the snipe and close the window.
         ---@type string|string[]
         cancel_snipe = { "<esc>", "q" },
       },
