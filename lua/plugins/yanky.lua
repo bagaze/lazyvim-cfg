c5e96629caa555d010427c7f4f047594df88adc8
Baptiste GAZE <bpt.gaze@gmail.com>
2026-09-21 14:53:36 +0200

    update: yanky - snacks ring history

--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -12,6 +12,8 @@ return {
       function()
         if LazyVim.pick.picker.name == "telescope" then
           require("telescope").extensions.yank_history.yank_history({})
+        elseif LazyVim.pick.picker.name == "snacks" then
+          Snacks.picker.yanky()
         else
           vim.cmd([[YankyRingHistory]])
         end
